de4f3989160330ad7514b669daa892ff711efbe4
Erik Bohne <eriknbohne@gmail.com>
2023-09-07 12:04:40 +0200

    pset 1

--- pset1/main.m
+++ pset1/main.m
@@ -1,6 +1,20 @@
 % Assign value for µ
-mu = 5;
+mu = 1;
 
+% Define constants
+tsim = 20;
+x1_0 = 0.01;
+x2_0 = 0.01;
+
+% Start simulation
+sim('model.slx');
+
+% Plot graph
+figure;
+plot(ans.values.Data);
+xlabel('x1');
+ylabel('x2');
+title('Van der Pol Oscillator');
 
 
 

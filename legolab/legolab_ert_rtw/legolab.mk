3f51e21aca3e58e31c7028cfc392071091200d89
Erik Bohne <eriknbohne@gmail.com>
2023-10-03 15:17:47 +0200

    finished legolab

--- legolab/legolab_ert_rtw/legolab.mk
+++ legolab/legolab_ert_rtw/legolab.mk
@@ -2,7 +2,7 @@
 ## Makefile generated for component 'legolab'. 
 ## 
 ## Makefile     : legolab.mk
-## Generated on : Tue Oct 03 12:54:16 2023
+## Generated on : Tue Oct 03 15:16:46 2023
 ## Final product: $(RELATIVE_PATH_TO_ANCHOR)/legolab
 ## Product type : executable
 ## 

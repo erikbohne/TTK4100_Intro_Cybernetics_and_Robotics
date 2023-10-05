46ad82c62b20c269f6d61f093dd62c5a04a02fea
Erik Bohne <eriknbohne@gmail.com>
2023-10-05 17:35:10 +0200

    pset3

---
+++ pset3/d.m
@@ -0,0 +1,28 @@
+% Euler's method for AUV 
+% Define values
+u = 500; % [N]
+m = 200; % [kg]
+k = 100; % [kg/s]
+h = 0.1; % time step
+t = 0:h:15; % range of t from t = 0 to t = 15
+v = zeros(size(t));
+v(1) = 0; % v(0)
+
+% Iterate over all values of t
+for n = 1:numel(t) - 1
+
+    % Define function
+    v_dot = u/m - (k/m) * v(n);
+    
+    % Find next v value
+    v(n + 1) = v(n) + h * v_dot;
+   
+end
+
+% Plot velocity
+plot(t, v, "-")
+xlabel('Time [s]')
+ylabel('Velocity [m/s]')
+title('Velocity of AUV using Euler''s Method')
+
+

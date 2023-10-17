da630bd7f6af74b61c0944d081c9aab9159e18d5
Erik Bohne <eriknbohne@gmail.com>
2023-10-17 12:26:24 +0200

    rocket landing project

---
+++ rocket_landing/rocket.m
@@ -0,0 +1,12 @@
+% Define values
+m = 25000; % kg (weight of booster to land)
+F_th = 845000; % N (per Merlin motor)
+h0 = 10000; % Starting height
+k = 5.47; % Air resistance constant
+v_term = -211; % Terminal velocity
+v_ref = -10; % Landing velocity
+
+% Regulator values
+Kp = 1;
+Ki = 1;
+Kd = 1;
